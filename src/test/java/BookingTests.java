b1ce66430c64effdc4cdce7fea079783662d9b20
LorenaCQ <lorenacq07@gmail.com>
2023-01-27 19:10:30 -0300

    Update gerar checkin e checkout com java faker

--- src/test/java/BookingTests.java
+++ src/test/java/BookingTests.java
@@ -37,13 +37,7 @@ public class BookingTests {
                 faker.internet().password(8,10),
                 faker.phoneNumber().toString());
 
-        /*Gera uma data checkin igual a data atual e a data checkout é gerada a partir da data checkin
-          somando dias aleatórios entre 1 e 5 dias. E então usa o método toString() para converter em String
-          no formato "yyyy-MM-dd". */
-        LocalDate checkin = LocalDate.now();
-        LocalDate checkout = checkin.plusDays(faker.number().numberBetween(1,5));
 
-        bookingDates = new BookingDates(checkin.toString(), checkout.toString());
         booking = new Booking(user.getFirstName(), user.getLastName(),
                 (float)faker.number().randomDouble(2, 50, 100000),
                 true,bookingDates,
